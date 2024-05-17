733308ddc6e47c623b5b4bd0ee90ba4cb98dde5c
snofamv <fniclous97@gmail.com>
2024-05-17 00:42:59 -0400

    updates navbar styles

--- src/components/Nabvar/Navbar.tsx
+++ src/components/Nabvar/Navbar.tsx
@@ -4,14 +4,15 @@ import { useContext } from "react";
 import styles from "./Nabbar.module.css";
 
 export const NavbarDashboard = ({ inputComponent: Component, handleInput }) => {
-  // Renombré inputComponent a Component para seguir la convención de nombres de componentes con mayúscula inicial
-  const authContext = useContext(AuthContext)!;
+  const authContext = useContext(AuthContext);
   const { logout } = authContext;
   const navigate = useNavigate();
+
   const handleLogout = () => {
     navigate("/");
     logout();
   };
+
   return (
     <>
       <nav
@@ -33,7 +34,7 @@ export const NavbarDashboard = ({ inputComponent: Component, handleInput }) => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
               <li className="nav-item">
                 <NavLink to={"/"} className="nav-link active">
                   <div>
@@ -44,55 +45,101 @@ export const NavbarDashboard = ({ inputComponent: Component, handleInput }) => {
                   </div>
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to={"/dueños"} className="nav-link">
-                  <div>
-                    <span>
-                      <i></i>
-                    </span>
-                    <span>Ver dueños</span>
-                  </div>
-                </NavLink>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Empleados
+                </a>
+                <ul className="dropdown-menu">
+                  <li className="dropdown-item">
+                    {" "}
+                    <NavLink to={"/nuevo-empleado"} className="nav-link">
+                      <div>
+                        <span>
+                          <i></i>
+                        </span>
+                        <span>Nueva empleado</span>
+                      </div>
+                    </NavLink>
+                  </li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <NavLink to={"/mascotas"} className="nav-link active">
-                  <div>
-                    <span>
-                      <i></i>
-                    </span>
-                    <span>Ver mascotas</span>
-                  </div>
-                </NavLink>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dueños mascotas
+                </a>
+                <ul className="dropdown-menu">
+                  <li className="dropdown-item">
+                    <NavLink to={"/dueños"} className="nav-link">
+                      <div>
+                        <span>
+                          <i></i>
+                        </span>
+                        <span>Ver dueños</span>
+                      </div>
+                    </NavLink>
+                  </li>
+                  <li className="dropdown-item">
+                    {" "}
+                    <NavLink
+                      to={"/registrar/titular-mascota"}
+                      className="nav-link"
+                    >
+                      <div>
+                        <span>
+                          <i></i>
+                        </span>
+                        <span>Registrar dueño</span>
+                      </div>
+                    </NavLink>
+                  </li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <NavLink to={"/nueva-mascota"} className="nav-link">
-                  <div>
-                    <span>
-                      <i></i>
-                    </span>
-                    <span>Nueva mascota</span>
-                  </div>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to={"/nuevo-empleado"} className="nav-link">
-                  <div>
-                    <span>
-                      <i></i>
-                    </span>
-                    <span>Nueva empleado</span>
-                  </div>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to={"/registrar/titular-mascota"} className="nav-link">
-                  <div>
-                    <span>
-                      <i></i>
-                    </span>
-                    <span>Registrar dueño mascota</span>
-                  </div>
-                </NavLink>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Mascotas
+                </a>
+                <ul className="dropdown-menu">
+                  <li className="dropdown-item">
+                    <NavLink to={"/mascotas"} className="nav-link active">
+                      <div>
+                        <span>
+                          <i></i>
+                        </span>
+                        <span>Ver mascotas</span>
+                      </div>
+                    </NavLink>
+                  </li>
+                  <li className="dropdown-item">
+                    <NavLink to={"/nueva-mascota"} className="nav-link">
+                      <div>
+                        <span>
+                          <i></i>
+                        </span>
+                        <span>Nueva mascota</span>
+                      </div>
+                    </NavLink>
+                  </li>
+                </ul>
               </li>
               <li className="nav-item">
                 <NavLink to={"/"} className="nav-link" onClick={handleLogout}>
