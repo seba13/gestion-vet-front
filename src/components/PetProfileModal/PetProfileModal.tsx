eb1cb11f13d6ce06b7a22f9baac974ab57b8001b
snofamv <fniclous97@gmail.com>
2024-05-21 19:11:17 -0400

    fixes

--- src/components/PetProfileModal/PetProfileModal.tsx
+++ src/components/PetProfileModal/PetProfileModal.tsx
@@ -7,6 +7,10 @@ import Tab from "react-bootstrap/Tab";
 import PetHistoryMedModal from "./PetHistoryMedModal";
 import PetCitasModal from "./PetCitasModal";
 import "./PetProfileModal.css"; // Import the CSS file
+import { NavLink } from "react-router-dom";
+import { parseDate } from "../../utils/utils";
+import { EstadosCita, IAppointment } from "../../interfaces/Appointment";
+import useFetch from "../../hooks/useFetch";
 
 interface PetProfileModalProps {
   idMascota: string;
@@ -26,6 +30,18 @@ const handleFetchPet = async (parametro: string) => {
   }
 };
 
+const fetchHistorialMedico = async (idMascota: string) => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/historial-medico/${idMascota}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    console.error("Error fetching historial medico:", error.message);
+  }
+};
+
 const PetProfileModal: React.FC<PetProfileModalProps> = ({
   idMascota,
   show,
@@ -34,6 +50,17 @@ const PetProfileModal: React.FC<PetProfileModalProps> = ({
   const [petInformation, setPetInformation] = useState<Pet | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [isLoadingHistorial, setIsLoadingHistorial] = useState(false);
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
+  const { fetchData, loading } = useFetch(
+    `${import.meta.env.VITE_API_URL}/citas-medicas/mascota/${idMascota}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   useEffect(() => {
     if (show) {
       setIsLoading(true);
@@ -47,6 +74,22 @@ const PetProfileModal: React.FC<PetProfileModalProps> = ({
     }
   }, [idMascota, show]);
 
+  const handleHistorialClick = () => {
+    setIsLoadingHistorial(true);
+    const fetchHistorial = async () => {
+      const response = await fetchHistorialMedico(idMascota);
+      setHistorialMedico(response.data);
+      setIsLoadingHistorial(false);
+    };
+
+    fetchHistorial();
+  };
+  const handleAppointmentsClick = () => {
+    fetchData().then((result) => {
+      setAppointments(result.data);
+    });
+  };
+
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
@@ -59,6 +102,13 @@ const PetProfileModal: React.FC<PetProfileModalProps> = ({
             defaultActiveKey="profile"
             id="pet-profile-tabs"
             className="mb-3"
+            onSelect={(eventKey) => {
+              if (eventKey === "historial") {
+                handleHistorialClick();
+              } else if (eventKey === "citas") {
+                handleAppointmentsClick();
+              }
+            }}
           >
             <Tab eventKey="profile" title="Perfil">
               <div className="pet-profile">
@@ -87,7 +137,55 @@ const PetProfileModal: React.FC<PetProfileModalProps> = ({
               <PetHistoryMedModal idMascota={idMascota} />
             </Tab>
             <Tab eventKey="citas" title="Citas">
-              <PetCitasModal idMascota={idMascota} />
+              {/* <PetCitasModal idMascota={idMascota} /> */}
+              {loading && <p className="p text-center">Cargando citas....</p>}
+              {!loading && (
+                <div className="">
+                  <NavLink
+                    to={`/citas/agendar/${petInformation.idMascota}`}
+                    className="btn btn-primary d-block w-25 mb-2"
+                  >
+                    Agendar🕛
+                  </NavLink>
+                  <ul className="list-group gap-1">
+                    {appointments.map((appointment: IAppointment) => {
+                      return (
+                        <li
+                          key={appointment.idCitaMedica}
+                          className={`d-flex justify-content-between list-group-item list-group-item-${
+                            appointment.idEstadoCita === 1
+                              ? "success"
+                              : appointment.idEstadoCita === 2
+                              ? "danger"
+                              : appointment.idEstadoCita === 3
+                              ? "warning"
+                              : "secondary"
+                          } `}
+                        >
+                          <div>
+                            <p>
+                              Fecha:{" "}
+                              <b>{parseDate(appointment.fechaCitaMedica)}</b>
+                              <br />
+                              Estado cita:{" "}
+                              <b>{EstadosCita[appointment.idEstadoCita]}</b>
+                            </p>
+                          </div>
+
+                          <div>
+                            <NavLink
+                              to={`/citas/editar/${appointment.idCitaMedica}`}
+                              className="btn btn-primary w-10"
+                            >
+                              Editar✏️🕛
+                            </NavLink>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              )}
             </Tab>
           </Tabs>
         )}
